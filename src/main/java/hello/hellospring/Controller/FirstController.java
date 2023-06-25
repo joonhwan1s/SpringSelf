be54e16301910f0b178b73d24c3d7c64c4a7a687
joonhwan1s <50772383+joonhwan1s@users.noreply.github.com>
2023-06-25 14:15:47 +0900

    basecommit

---
+++ src/main/java/hello/hellospring/Controller/FirstController.java
@@ -0,0 +1,14 @@
+package hello.hellospring.Controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+
+public class FirstController {
+
+    @Controller
+    public String hello(Model model){
+        model.addAttribute("data","hello");
+        return "hello";
+    }
+
+}
